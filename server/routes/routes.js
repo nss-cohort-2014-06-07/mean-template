12d23cf66f3f39019037636aa478cedc0e02245c
Chyld Medford <chyld.medford@gmail.com>
2014-09-10 20:27:42 -0500

    body parse json

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -12,6 +12,7 @@ module.exports = function(app, express){
   app.use(morgan('dev'));
   app.use(express.static(__dirname + '/../../public'));
   app.use(bodyParser.urlencoded({extended:true}));
+  app.use(bodyParser.json());
   app.use(methodOverride());
   app.use(session({store:new RedisStore(), secret:'my super secret key', resave:true, saveUninitialized:true, cookie:{maxAge:null}}));
 
