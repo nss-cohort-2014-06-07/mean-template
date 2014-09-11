077ff996d056466264b8f155aa72394c8b42e1b8
Chyld Medford <chyld.medford@gmail.com>
2014-09-11 07:50:20 -0500

    home to Home

--- client/components/home.js
+++ client/components/home.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('mean-template')
-  .factory('home', ['$http', function($http){
+  .factory('Home', ['$http', function($http){
 
     function getMessage(){
       return $http.get('/home');
--- client/views/home/home.js
+++ client/views/home/home.js
@@ -2,8 +2,8 @@
   'use strict';
 
   angular.module('mean-template')
-  .controller('HomeCtrl', ['$scope', '$interval', 'home', function($scope, $interval, home){
-    home.getMessage().then(function(response){
+  .controller('HomeCtrl', ['$scope', '$interval', 'Home', function($scope, $interval, Home){
+    Home.getMessage().then(function(response){
       $scope.mean = response.data.mean;
 
       $interval(function(){
