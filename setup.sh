7fd4fb6933a44e28ba90d3430a3c2310265327fd
Chyld Medford <chyld.medford@gmail.com>
2014-09-12 23:01:31 -0500

    bump angular version

--- setup.sh
+++ setup.sh
@@ -41,8 +41,8 @@ npm install grunt-shell --save-dev
 npm install jshint-stylish --save-dev
 
 # installing new bower components
-bower install angular#1.3.0-rc.0 --save
-bower install angular-route#1.3.0-rc.0 --save
+bower install angular#1.3.0-rc.1 --save
+bower install angular-route#1.3.0-rc.1 --save
 bower install bootstrap --save
 bower install fontawesome --save
 bower install underscore --save
